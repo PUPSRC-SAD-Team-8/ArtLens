1d845304be1ba25354a39e1468f2db161f063652
___Sundae <jennymaelbaldoza@iskolarngbayan.pup.edu.ph>
2024-07-04 15:35:06 +0800

    ci: updates to testing credentials
    however the credentials are exposed to public

--- connection.php
+++ connection.php
@@ -1,11 +1,10 @@
-<?php 
+<?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "artlens";
+$username = 'u155023598_artlensKvufe';
+$password = 'dZNtUjukGYb4C55zqGmtvZJBzqWKeFbYRnJkwJXDV4467xfa4wrcZqCZLHUgv5vA';
+$dbname = "u155023598_artlens";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-if($conn->connect_error){
-    die("Failed Connection".$conn->connect_error);
+if ($conn->connect_error) {
+    die("Failed Connection" . $conn->connect_error);
 }
-?>

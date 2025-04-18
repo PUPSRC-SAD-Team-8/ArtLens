9bb7f407d4b6d6abed10394447bf792e462d8f73
C-Ivan-Bandilla <jennymaelbaldoza@iskolarngbayan.pup.edu.ph>
2025-04-19 01:00:45 +0800

    ci: correction to v1 prerelease

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 const config = {
     branches: [
         // Pre-releases for v1 (continue using 'alpha' from previous tags)
-        'v1', { name: 'v1-dev', prerelease: 'alpha' },
+        'v1', { name: 'v1-dev', prerelease: 'alpha.28' },
         'v2', { name: 'v2-dev', prerelease: 'v2.alpha' },
     ],
     // plugins: [

e2d64a7330d0540a097757b70d6582a7f596891f
c-ivan-bandilla <c.ivanbandilla+github@gmail.com>
2025-04-20 22:25:25 +0800

    ci: fix the release config to common js due to type module issues

--- release.config.cjs
+++ release.config.cjs
@@ -19,6 +19,7 @@ const config = {
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
+        "@semantic-release/npm",
         [
             "@semantic-release/github",
             {
